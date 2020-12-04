6690f021aa3b91495d26922c2b80b41f40740d0a
Spencer Owen <owenspencer@gmail.com>
2020-12-04 15:41:13 -0700

    multiple slopes

--- 2020/3a.rb
+++ 2020/3a.rb
@@ -6,26 +6,29 @@ end
 # puts input[0].join
 column_index=0
 row_index=0
-slope=[3,1]
+slopes=[[3,1]]
 
 trees = 0
+slopes.each do |slope|
+    (0...input.length).step(slope[1]) do | row|
+    if input[row][column_index] == '.'
+        input[row][column_index] = 'O'
+    elsif input[row][column_index] == '#'
+        input[row][column_index] = 'X'
+        trees = trees +1
+    else
+        raise "invalid value '#{input[row][column_index]}'"
+        # break
+    end 
+    column_index = column_index + slope[0]
+    column_index = column_index - 31 if column_index >= 31
+    end
 
-0.upto(input.length - 1) do | row|
-  if input[row][column_index] == '.'
-    input[row][column_index] = 'O'
-  elsif input[row][column_index] == '#'
-      input[row][column_index] = 'X'
-      trees = trees +1
-  else
-    raise "invalid value '#{input[row][column_index]}'"
-    # break
-  end 
-  column_index = column_index + slope[0]
-  column_index = column_index - 31 if column_index >= 31
+
+    puts "==============="
+    input.each do |i|
+        puts i.join
+    end
 end
-
-
-puts "==============="
-puts input.join
 
 puts "trees = #{trees}"
